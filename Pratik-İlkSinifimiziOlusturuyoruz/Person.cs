625c1429d7aec17175315c9d9638cdb21627d066
sburakozdemir <sburakozdemir00@gmail.com>
2024-08-06 12:51:28 +0300

    Add project files.

---
+++ Pratik-İlkSinifimiziOlusturuyoruz/Person.cs
@@ -0,0 +1,41 @@
+﻿// Person sınıfı, öğrenci ve öğretmen bilgilerini tutmak için kullanılır
+namespace Pratik_İlkSinifimiziOlusturuyoruz
+{
+    internal class Person
+    {
+        // Kişinin adını tutan özel değişken
+        private string name;
+        // Kişinin soyadını tutan özel değişken
+        private string surname;
+        // Kişinin doğum tarihini tutan özel değişken
+        private DateTime dateOfBirth;
+
+        // Ad özelliği için public property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Soyad özelliği için public property
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        // Doğum tarihi özelliği için public property
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set { dateOfBirth = value; }
+        }
+
+        // Kişinin bilgilerini konsola yazdıran metot
+        public void Yazdır()
+        {
+            Console.WriteLine($"Adı: {name} \nSoyadı: {surname} \nDoğum Tarihi: {dateOfBirth.ToShortDateString()}");
+            Console.WriteLine("**********************************************************");
+        }
+    }
+}
---
+++ Pratik-İlkSinifimiziOlusturuyoruz/Program.cs
@@ -0,0 +1,29 @@
+﻿using Pratik_İlkSinifimiziOlusturuyoruz;
+
+// İlk öğretmen nesnesini oluştur ve bilgilerini ata
+Person teacher1 = new Person();
+teacher1.Name = "Berk";
+teacher1.Surname = "Bayındır";
+teacher1.DateOfBirth = new DateTime(1989, 9, 9);
+teacher1.Yazdır();
+
+// İkinci öğretmen nesnesini oluştur ve bilgilerini ata
+Person teacher2 = new Person();
+teacher2.Name = "Ufuk";
+teacher2.Surname = "Yıkılmaz";
+teacher2.DateOfBirth = new DateTime(1986, 12, 27);
+teacher2.Yazdır();
+
+// İlk öğrenci nesnesini oluştur ve bilgilerini ata
+Person student1 = new Person();
+student1.Name = "Aşkın";
+student1.Surname = "Şalcıoğlu";
+student1.DateOfBirth = new DateTime(2000, 2, 14);
+student1.Yazdır();
+
+// İkinci öğrenci nesnesini oluştur ve bilgilerini ata
+Person student2 = new Person();
+student2.Name = "Can";
+student2.Surname = "Serbes";
+student2.DateOfBirth = new DateTime(2000, 5, 18);
+student2.Yazdır();
